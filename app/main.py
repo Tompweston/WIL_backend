28e555b0314f3ba0cb5d001cff8805eb4c325ba0
Tom Weston <tomowest23@icloud.com>
2025-08-07 15:23:19 +0800

    Added scalar docs

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from app.config.database import init
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+from scalar_fastapi import get_scalar_api_reference
+from app.router.tasks import router   
+
 
 app = FastAPI()
+
+@app.get("/scalar", include_in_schema=False)
+async def scalar_html():
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        title=app.title,
+    )
 
 @app.on_event("startup")
 async def start_db():
@@ -13,6 +21,6 @@ async def start_db():
 async def homepage():
     return {"message": "Welcome to the To-Do List API!"}    
 
-
+app.include_router(router) #Include the tasks router
 #fastapi dev main.py -- is the command to run the FastAPI application'''
 #uv run fastapi run -- command to run the FastAPI application
