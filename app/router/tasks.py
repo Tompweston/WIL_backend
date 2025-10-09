29e424d66e7c4c2efbb7f9a07e77cf5f7cb6a358
Tom Weston <tomowest23@icloud.com>
2025-10-09 15:38:06 +0800

    all requests are now authenticated against the server for valid session and session expiry

--- app/router/tasks.py
+++ app/router/tasks.py
@@ -5,32 +5,17 @@ from beanie import PydanticObjectId
 from typing import List
 from bson import ObjectId
 from datetime import datetime
+from fastapi import FastAPI, Header
+from typing import Annotated
 
-#places task routes under the /tasks prefix and tags them as "Tasks" in the API documentation
-tasks_router = APIRouter( tags=["Tasks"] )    
 
+# Authenticates user based on session validity and expiry
+async def authenticate_user(user_id: str, type: str) -> bool:
+    print ("\nAUTHENTICATION\n----------------")
 
-# Create a new task
-@tasks_router.post("/", response_model=Task)
-async def create_task(task: Task):
-    await task.create()
-    return task
-
-# Get all tasks
-@tasks_router.get("/", response_model=List[Task])
-async def get_all_tasks():
-    tasks = await Task.find_all().to_list()
-    if not tasks:
-        raise HTTPException(status_code=404, detail="No tasks found")
-    return tasks
-
-# Get tasks by user ID
-@tasks_router.get("/{user_id}", response_model=List[Task])
-async def get_tasks_by_user_id(user_id: str):
+    print("   Authenticating user...", user_id)
     user_obj_id = PydanticObjectId(user_id)
-    print(f"\nUser ID: {user_id}\n\n")
-    print(f"\nUser ID Comparison: {user_id_comparison}\n\n")
-    valid_session = await session.find({"userId": user_id_comparison}).to_list()
+    valid_session = await session.find({"userId": user_obj_id}).to_list()
     session_dicts = [
     {
         "id": str(s.id),
@@ -41,49 +26,110 @@ async def get_tasks_by_user_id(user_id: str):
     for s in valid_session
     ]
 
-    print(f"\nSession Dicts: {session_dicts}\n\n")
-    print(f"\nValid Document: {valid_session}\n\n")
-
     if not valid_session:
-        raise HTTPException(status_code=401, detail="Unauthorized")
-
-    tasks = await Task.find({"user_id": user_id}).to_list()
-    if not tasks:
-        raise HTTPException(status_code=404, detail="No tasks found for this user")
-    return tasks
+        print("   No valid session found.")
+        return False
     
+    if valid_session:
+        for s in session_dicts:
+            exp_time = datetime.fromisoformat(s["expiresAt"])
+            if exp_time < datetime.utcnow():
+                print("   Session expired at", exp_time)
+                await session.get(ObjectId(s["id"])).delete()
+                return False
+            else:
+                print("   Session is not expired and valid until", exp_time)
+        print("   User authenticated successfully for", type, "route")
+        print("----------------\n")
+    return True
 
-# Get a task by ID
-@tasks_router.get("/{id}", response_model=Task)
-async def get_task(id: str):
-    task = await Task.get(id)
-    if not task:
-        raise HTTPException(status_code=404, detail="Task not found")
-    return task
+#places task routes under the /tasks prefix and tags them as "Tasks" in the API documentation
+tasks_router = APIRouter( prefix="/tasks", tags=["Tasks"] )
+#==================================================================================================================
+# Create a new task
+@tasks_router.post("/", response_model=Task)
+async def create_task(task: Task, userID: Annotated[str, Header()] = None):
+    user_id = userID
+    validated = await authenticate_user(user_id, "create")
+    if not validated:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+        return {"message": "Not Authenticated"}
+    if validated:
+        print("POST A NEW TASK HEADER:", user_id, "\n")
+        task.user_id = user_id
+        await task.create()
+        return task
+
+#==================================================================================================================
+
+#Get tasks by user ID
+@tasks_router.get("/", response_model=List[Task])
+async def get_tasks_by_user_id(userID: Annotated[str, Header()] = None):
+    user_id = userID
+    validated = await authenticate_user(user_id, "read")
+    print("GET ALL TASKS HEADER:", user_id, "\n")
+    if not validated:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+        return {"message": "Not Authenticated"}
+    if validated:
+        tasks = await Task.find({"user_id": user_id}).to_list()
+        if not tasks:
+            raise HTTPException(status_code=404, detail="No tasks found for this user")
+        return tasks
+
+#==================================================================================================================
 
 # Update a task by ID
 @tasks_router.patch("/{id}", response_model=Task)
-async def update_task(id: str, task_data: TaskUpdate):
-    task = await Task.get(id)
-    if not task:
-        raise HTTPException(status_code=404, detail="Task not found")
-    recived = task_data.dict(exclude_unset=True)
-    for key, value in recived.items():
-        setattr(task, key, value)
-    await task.save()
-    return task
+async def update_task(id: str, task_data: TaskUpdate, userID: Annotated[str, Header()] = None):
+    user_id = userID
+    validated = await authenticate_user(user_id, "update")
+    print("\nUPDATE TASK HEADER:", user_id, "\n")
+    if not validated:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+        return {"message": "Not Authenticated"}
+    if validated:
+        task = await Task.get(id)
+        if not task:
+            raise HTTPException(status_code=404, detail="Task not found")
+        recived = task_data.dict(exclude_unset=True)
+        for key, value in recived.items():
+            setattr(task, key, value)
+        await task.save()
+        return task
+
+#==================================================================================================================
 
 # Delete a task by ID
 @tasks_router.delete("/{id}")
-async def delete_task(id: str):
-    task = await Task.get(id)
-    if not task:
-        raise HTTPException(status_code=404, detail="Task not found")
-    await task.delete()
-    return {"message": "Task deleted"}
+async def delete_task(id: str, userID: Annotated[str, Header()] = None):
+    user_id = userID
+    validated = await authenticate_user(user_id, "delete")
+    print("\nDELETE SINGLE TASK HEADER:", user_id, "\n")
+    if not validated:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+        return {"message": "Not Authenticated"}
+    if validated:
+        task = await Task.get(id)
+        if not task:
+            raise HTTPException(status_code=404, detail="Task not found")
+        await task.delete()
+        return {"message": "Task deleted"}
 
-# Delete all tasks
+#==================================================================================================================
+
+#delete tasks by user ID
 @tasks_router.delete("/")
-async def delete_all_tasks():
-    await Task.delete_all()
-    return {"message": "All tasks deleted"}
+async def delete_tasks_by_user_id(userID: Annotated[str, Header()] = None):
+    user_id = userID
+    print("\nDELETE ALL TASKS HEADER:", user_id, "\n")
+    validated = await authenticate_user(user_id, "delete all")
+    if not validated:
+        print("Not Authenticated delete")
+        raise HTTPException(status_code=401, detail="Unauthorized")
+        return {"message": "Not Authenticated"}
+    if validated:
+        await Task.find({"user_id": user_id}).delete_many()
+        return {"message": f"All tasks for user {user_id} deleted"}
+
+#================================================================================================================== 
